f77b25b39741f90258095028c03cb607ea0d0cdb
Aleksander Malcew <amalcew@protonmail.com>
2021-12-04 21:58:05 +0100

    Mockup v1.0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from math import sqrt
-
 from bokeh.io import curdoc
 from bokeh.layouts import row, column
 from bokeh.models import ColumnDataSource
@@ -7,132 +6,39 @@ from bokeh.models.widgets import Slider, RangeSlider, TextInput
 from bokeh.plotting import figure
 
 
-def regulator(_fluid_level, _tank_cross_section, _min_level, _max_level, _sampling, _regulator_amp, _inflow_factor_min,
-              _inflow_factor_max, _outflow_factor, _integral_action_time, _derivative_action_time, fluid_level_array,
-              error_array, inflow_array, signal_array):
-    """ PID regulator algorithm """
-
-    # Step 1: Calculate regulation error.
-    error = _fluid_level - fluid_level_array[-1]
-    error_array.append(error)
-
-    # Step 2: Calculate value of steering signal.
-    signal = _regulator_amp * (error_array[-1] + (_sampling / _integral_action_time) * sum(error_array) + (
-                (_derivative_action_time / _sampling) * (error_array[-2] - error_array[-1])))
-    signal_array.append(signal)
-
-    # Step 3: Calculate value of inflow intensity.
-    inflow = (_inflow_factor_max - _inflow_factor_min) / (_max_level - _min_level) * signal_array[-1]
-    inflow_array.append(inflow)
-
-    # Step 4: Calculate fluid level.
-    current_fluid_level = float(
-        1 / _tank_cross_section * ((-_outflow_factor) * sqrt(fluid_level_array[-1]) + inflow_array[-1]) * _sampling +
-        fluid_level_array[-1])
-    if current_fluid_level > _max_level:
-        current_fluid_level = _max_level
-    elif current_fluid_level < _min_level:
-        current_fluid_level = _min_level
-    fluid_level_array.append(current_fluid_level)
-
-    return current_fluid_level
-
-
-""" widgets """
-fluid_level_slider = Slider(title="Desired fluid level", value=5.0, start=0.0, end=10.0, step=0.5)
-level_range_slider = RangeSlider(title="Tank min/max size", value=(0, 10), start=0, end=100, step=1)
-tank_cross_section_text_input = TextInput(title="Tank Cross section area", value=str(1.5))
-simulation_time_text_input = TextInput(title="Simulation time", value=str(3600 * 4))
-
-sampling_slider = Slider(title="Sampling", value=0.10, start=0.05, end=1.0, step=0.05)
-regulator_amp_slider = Slider(title="Amplification", value=0.015, start=0.005, end=1.00, step=0.005, format='0.000')
-inflow_factor_range_slider = RangeSlider(title="Inflow factors", value=(0.00, 0.05), start=0.00, end=1.00, step=0.005,
-                                         format='0.000')
-outflow_factor_slider = Slider(title="Outflow factor", value=0.035, start=0.005, end=1.00, step=0.005, format='0.000')
-integral_action_time_text_input = TextInput(title="Integral action time factor", value=str(0.05))
-derivative_action_time_text_input = TextInput(title="Derivative action time factor", value=str(0.25))
-
-""" plot settings """
-plot = figure(title="Fluid Tank simulation",
-              tools="pan,reset,save,wheel_zoom",
-              x_range=[0, 3600 * 4], y_range=[0, 10], height=500, width=1200)
-plot.sizing_mode = 'scale_width'
-
-""" load default plot """
 file = open("data", "r")
 file = file.readlines()
 y = list(map(float, file))
 x = [i for i in range(len(y))]
 source = ColumnDataSource(data=dict(x=x, y=y))
-plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
 
-def update_data(attrname, old, new):
-    """ update callback """
+""" plots """
+plot_xy = figure(title="XY", tools="pan,reset,save,wheel_zoom", x_range=[-100, 100], y_range=[-100, 100])
+plot_xy.sizing_mode = 'stretch_both'
+plot_xy.line('x', 'y', source=ColumnDataSource(data=dict(x=[i for i in range(-100, 100)], y=[i for i in range(-100, 100)])))
 
-    # load widgets data
-    fluid_level = fluid_level_slider.value  # m
-    min_level = level_range_slider.value[0]  # m
-    max_level = level_range_slider.value[1]  # m
-    tank_cross_section = float(tank_cross_section_text_input.value)  # m^2
-    simulation_time = simulation_time_text_input.value  # s
+plot_x_axis = figure(title="X axis", tools="pan,reset,save,wheel_zoom")
+plot_x_axis.sizing_mode = 'stretch_both'
+plot_x_axis.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
-    sampling = sampling_slider.value  # s
-    regulator_amp = regulator_amp_slider.value
-    inflow_factor_min = inflow_factor_range_slider.value[0]  # m^3/s
-    inflow_factor_max = inflow_factor_range_slider.value[1]  # m^3/s
-    outflow_factor = outflow_factor_slider.value  # m^3/s
-
-    integral_action_time = float(integral_action_time_text_input.value)
-    derivative_action_time = float(derivative_action_time_text_input.value)
-
-    # generate regulator arrays
-    x_axis = []
-    y_axis = []
-    fluid_level_array = [float(min_level)]
-    error_array = [float(fluid_level)]
-    inflow_array = []
-    signal_array = []
-
-    # main simulation loop
-    for n in range(0, int(simulation_time)):
-        x_axis.append(n)
-        y_axis.append(
-            regulator(fluid_level, tank_cross_section, min_level, max_level, sampling, regulator_amp, inflow_factor_min,
-                      inflow_factor_max, outflow_factor, integral_action_time, derivative_action_time,
-                      fluid_level_array, error_array, inflow_array, signal_array))
-
-    # update plot
-    source.data = dict(x=x_axis, y=y_axis)
-
-    # update sliders and grid range
-    plot.y_range.start = min_level
-    plot.y_range.end = max_level
-    fluid_level_slider.start = min_level
-    fluid_level_slider.end = max_level
-    plot.x_range.end = int(simulation_time)
+plot_y_axis = figure(title="Y axis", tools="pan,reset,save,wheel_zoom")
+plot_y_axis.sizing_mode = 'stretch_both'
+plot_y_axis.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
 
-# column 1
-fluid_level_slider.on_change('value_throttled', update_data)
-level_range_slider.on_change('value_throttled', update_data)
-tank_cross_section_text_input.on_change('value', update_data)
-simulation_time_text_input.on_change('value', update_data)
+""" widgets """
+slider_1 = Slider(title="Slider 1", value=5.0, start=0.0, end=10.0, step=0.5)
+slider_2 = Slider(title="Slider 2", value=5.0, start=0.0, end=10.0, step=0.5)
+slider_3 = Slider(title="Slider 3", value=5.0, start=0.0, end=10.0, step=0.5)
+range_slider_1 = RangeSlider(title="Range slider 1", value=(0, 10), start=0, end=100, step=1)
+text_input_1 = TextInput(title="Text input 1", value=str(12.5))
+text_input_2 = TextInput(title="Text input 2", value=str(14.0))
+text_input_3 = TextInput(title="Text input 3", value=str(1.5))
 
-# column 2
-sampling_slider.on_change('value_throttled', update_data)
-regulator_amp_slider.on_change('value_throttled', update_data)
-inflow_factor_range_slider.on_change('value_throttled', update_data)
-outflow_factor_slider.on_change('value_throttled', update_data)
 
-# column 3
-integral_action_time_text_input.on_change('value', update_data)
-derivative_action_time_text_input.on_change('value', update_data)
-
-inputs_1 = column(fluid_level_slider, level_range_slider, tank_cross_section_text_input, simulation_time_text_input,
-                  sizing_mode='scale_width')
-inputs_2 = column(sampling_slider, regulator_amp_slider, inflow_factor_range_slider, outflow_factor_slider,
-                  sizing_mode='scale_width')
-inputs_3 = column(integral_action_time_text_input, derivative_action_time_text_input, sizing_mode='scale_width')
-curdoc().add_root(column(plot, row(inputs_1, inputs_2, inputs_3), sizing_mode='scale_width'))
-curdoc().title = "PID regulator example"
+curdoc().add_root(row(
+    column(slider_1, slider_2, slider_3, range_slider_1, row(text_input_1, text_input_2, text_input_3)),
+    column(plot_xy, row(plot_x_axis, plot_y_axis), sizing_mode='stretch_both'),
+    sizing_mode='stretch_both'))
+curdoc().title = "Self-balancing table simulation mockup"
